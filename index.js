9579e660d5ec8a64b26600a5e0347da79c16fb68
talaltaki <talaltaki.tt@gmail.com>
2020-10-13 17:09:35 +0600

    Added Services & Feedbacks from Server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("bodyParser");
-
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9sjrg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
@@ -16,9 +17,27 @@ app.get("/", (req, res) => {
   res.send("Server of Creative Agency");
 });
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 client.connect((err) => {
   const serviceCollection = client.db("creativeAgency").collection("services");
+  const feedbackCollection = client
+    .db("creativeAgency")
+    .collection("feedbacks");
+
+  app.get("/services", (req, res) => {
+    serviceCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
+  app.get("/feedbacks", (req, res) => {
+    feedbackCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
 });
 
 app.listen(process.env.PORT || port);
